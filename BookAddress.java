0886949d64c188fa58aa5827388f5cf5900ad90d
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-09-22 12:44:20 +0530

    ADD:Contact details added

--- BookAddress.java
+++ BookAddress.java
@@ -1,5 +1,5 @@
 package www.bridgelabz.demobook;
-
+import java.util.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,5 +10,22 @@ public class BookAddress
     public static void main( String[] args )
     {
         LOG.info( "Wecome to address Book Program" );
+        Scanner value=new Scanner (System.in);
+        int PhoneNumber;
+        String Address,FirstName,lastName;
+        LOG.info( "Enter the first name" );
+        FirstName=value.nextLine();
+        LOG.info( "Enter the last name" );
+        lastName=value.nextLine();
+        LOG.info( "Enter the Address name" );
+        Address=value.nextLine();
+        LOG.info( "Enter the phone number" );
+        PhoneNumber=value.nextInt();
+        LOG.info( "fistName" +FirstName);
+        LOG.info( "LastName" +lastName);
+        LOG.info( "address"+Address );
+        LOG.info( "PhoneNumber"+PhoneNumber );
+        
+       
     }
 }
