51be55af81e4f459016df2ebd99c81662ad6539c
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-09-22 12:20:32 +0530

    ADD:wecome message added

---
+++ BookAddress.java
@@ -0,0 +1,14 @@
+package www.bridgelabz.demobook;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class BookAddress 
+{
+	private static final Logger LOG= LogManager.getLogger("App.class"); 
+    public static void main( String[] args )
+    {
+        LOG.info( "Wecome to address Book Program" );
+    }
+}
