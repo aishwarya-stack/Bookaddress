5db0fa4da377c22fbba55e83541167408414e066
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-09-24 18:14:01 +0530

    ADD:NewContactListUpdated

--- BookAddress.java
+++ BookAddress.java
@@ -1,31 +1,86 @@
 package www.bridgelabz.demobook;
+
 import java.util.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+class Contactdetails {
 
-public class BookAddress 
-{
-	private static final Logger LOG= LogManager.getLogger("App.class"); 
-    public static void main( String[] args )
-    {
-        LOG.info( "Wecome to address Book Program" );
-        Scanner value=new Scanner (System.in);
-        int PhoneNumber;
-        String Address,FirstName,lastName;
-        LOG.info( "Enter the first name" );
-        FirstName=value.nextLine();
-        LOG.info( "Enter the last name" );
-        lastName=value.nextLine();
-        LOG.info( "Enter the Address name" );
-        Address=value.nextLine();
-        LOG.info( "Enter the phone number" );
-        PhoneNumber=value.nextInt();
-        LOG.info( "fistName" +FirstName);
-        LOG.info( "LastName" +lastName);
-        LOG.info( "address"+Address );
-        LOG.info( "PhoneNumber"+PhoneNumber );
-        
-       
-    }
+	private static final Logger LOG = LogManager.getLogger("BookAddress.class");
+	Integer[] phoneNumber = new Integer[10];
+	String[] Address = new String[10];
+	String[] Firstname = new String[10];
+	String[] Lastname = new String[10];
+
+	public void getValue(int phonenumber, String address, String firstname, String lastname, int length, int time) {
+		int count = 0;
+
+		for (int i = time; i < length; i++) {
+			phoneNumber[i] = phonenumber;
+			Address[i] = address;
+			Firstname[i] = firstname;
+			Lastname[i] = lastname;
+			// count=count+1;
+		}
+		LOG.info("phoneNumber" + Arrays.toString(phoneNumber));
+		LOG.info("Address" + Arrays.toString(Address));
+		LOG.info("Firstname" + Arrays.toString(Firstname));
+		LOG.info("Lastname" + Arrays.toString(Lastname));
+
+	}
+
+}
+
+public class BookAddress extends Contactdetails {
+	private static final Logger LOG = LogManager.getLogger("BookAddress.class");
+	private static final String NULL = null;
+
+	public static void main(String[] args) {
+		LOG.info("Welcome to address Book Program");
+
+		Scanner value = new Scanner(System.in);
+
+		BookAddress contact = new BookAddress();
+
+		// LOG.info("1.ADD 2.REPLACE 3.DELETE");
+		// 1LOG.info("enter Your Choice");
+		int Check = value.nextInt();
+		int choice;
+		do {
+			LOG.info("1.ADD 2.REPLACE 3.DELETE");
+			LOG.info("enter Your Choice");
+			Check = value.nextInt();
+			switch (Check) {
+			case 1:
+				LOG.info("enter the number of contact you want to add:");
+				int length = value.nextInt();
+				for (int i = 0; i < length; i++) {
+					LOG.info("enter the phonenumber");
+					int phonenumber = value.nextInt();
+
+					LOG.info("enter the address");
+					String address = value.nextLine();
+
+					LOG.info("enter the firstname");
+					String firstname = value.nextLine();
+
+					LOG.info("enter the lastname");
+					String lastname = value.nextLine();
+
+					int time = i;
+					contact.getValue(phonenumber, address, firstname, lastname, length, time);
+				}
+				// contact.getValue(phonenumber,address,firstname,lastname);
+				break;
+
+			default:
+				// empHr = 0;
+				break;
+
+			}
+			LOG.info("do you want to continue? 1.yes 2.No");
+			choice = value.nextInt();
+		} while (choice == 1);
+
+	}
 }
---
+++ JunitTest.java
@@ -0,0 +1,10 @@
+package www.bridgelabz.demobook;
+
+public class JunitTest{
+
+	public int add(int i,int j)
+	{
+	int sum=i+j;
+	return sum;
+}
+}
