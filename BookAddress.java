0f166adae8c1fb7d9dbec2fe2b879330e99663df
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-10-24 21:59:59 +0530

    Add:Maintain Dictionary of city and person and state and person

--- BookAddress.java
+++ BookAddress.java
@@ -208,6 +208,31 @@ public  class BookAddress {
 				System.out.println(object.first_name + " " + object.last_name);
 			}
 		}
+	}
 
+	public void viewPersonByState(String state) {
+		Map<String, String> stateMap = new HashMap<String, String>();
+		for (int j = 0; j < contact.size(); j++) {
+			BookAddress object = contact.get(j);
+			stateMap.put(object.first_name, object.state);
+		}
+		for (Map.Entry m : stateMap.entrySet()) {
+			if (m.getValue().equals(state)) {
+				System.out.println(m.getKey());
+			}
+		}
+	}
+
+	public void viewPersonByCity(String city) {
+		Map<String, String> cityMap = new HashMap<String, String>();
+		for (int j = 0; j < contact.size(); j++) {
+			BookAddress object = contact.get(j);
+			cityMap.put(object.first_name, object.city);
+		}
+		for (Map.Entry m : cityMap.entrySet()) {
+			if (m.getValue().equals(city)) {
+				System.out.println(m.getKey());
+			}
+		}
 	}
 }
